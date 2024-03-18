68c7ebc3ddbedc722fc1c7265ab1ca7def564bee
Zhakypovvv1 <145824451+Zhakypovvv1@users.noreply.github.com>
2024-03-18 23:44:53 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,4 @@ const nav = document.querySelector('.nav')
 burger.addEventListener('click', () => {
     nav.classList.toggle('slide-nav')
     burger.classList.toggle('burger-btn')
-    fetch(`https://api.telegram.org/bot7108491570:AAFsFTR6-NsrxfJGOoT_QqBIfj44W_DKblA/sendMessage?chat_id=1422432300&text=${message.value} ${tel.value}`)
 })
